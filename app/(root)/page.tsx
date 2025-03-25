154e988a6b804c43af0d95a72d5699522ad8cda7
iovel-mer <ioveeel.mind@gmail.com>
2025-03-25 14:26:31 +0400

    Started project,added some components

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+export const metadata = {
+  title: 'Home'
+}
+const Home = () => {
+  return ( <>
+  Home
+  </> );
+}
+ 
+export default Home;
---
+++ lib/constants/index.ts
@@ -0,0 +1,4 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Store'
+export const APP_DESC = process.env.NEXT_PUBLIC_APP_DESC || 'Modern ecommerce'
+
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
