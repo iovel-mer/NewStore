b956a4b535ed182334d980cdbe65c0f1dec336b3
iovel-mer <ioveeel.mind@gmail.com>
2025-03-27 21:07:19 +0400

    rrrrr

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,41 +1,31 @@
-import { APP_NAME } from "@/lib/constants";
-import Image from "next/image";
-import Link from "next/link";
-import { Button } from "../../ui/button";
-import { ShoppingCart, UserIcon } from "lucide-react";
-import ModeToggle from "./mode-toggle";
+import Image from 'next/image';
+import Link from 'next/link';
+import { APP_NAME } from '@/lib/constants';
+import Menu from './menu';
+
 
 const Header = () => {
   return (
-    <header className="w-full border-b">
-      <div className="wrapper flex-between">
-        <div className="flex-start">
-          <Link href="/" className="flex-start">
+    <header className='w-full border-b'>
+      <div className='wrapper flex-between'>
+        <div className='flex-start'>
+          <Link href='/' className='flex-start ml-4'>
             <Image
-              src="/images/logo.svg"
+              src='/images/logo.svg'
               alt={`${APP_NAME} logo`}
               height={48}
               width={48}
               priority={true}
             />
-            <span className="hidden lg:block font-bold text-2xl ml-3">
+            <span className='hidden lg:block font-bold text-2xl ml-3'>
               {APP_NAME}
             </span>
           </Link>
         </div>
-        <div className="space-x-2">
-          <ModeToggle/>
-          <Button asChild variant="ghost">
-            <Link href="/cart">
-              <ShoppingCart /> Cart
-            </Link>
-          </Button>
-          <Button asChild>
-            <Link href="/sign-in">
-              <UserIcon /> Sign in
-            </Link>
-          </Button>
+        <div className='hidden md:block'>
+         
         </div>
+        <Menu />
       </div>
     </header>
   );
